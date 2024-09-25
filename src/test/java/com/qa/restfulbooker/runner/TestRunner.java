bfb1a897f772344918f722d8d7ac81a1f94e6a27
Adarsh <101453175+Adarsh409@users.noreply.github.com>
2024-09-25 11:23:36 +0530

    Update TestRunner.java

--- src/test/java/com/qa/restfulbooker/runner/TestRunner.java
+++ src/test/java/com/qa/restfulbooker/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".//Features", glue = { "com.qa.restfulbooker.stepdefs" }, plugin = {
-		"json:target/jsonReports/cucmber-reports.json","pretty", "html:test-output.html" }, dryRun = false, tags="@CreateBooking")
+		"json:target/jsonReports/cucmber-reports.json","pretty", "html:test-output.html" }, dryRun = false)
 public class TestRunner {
 
 }
