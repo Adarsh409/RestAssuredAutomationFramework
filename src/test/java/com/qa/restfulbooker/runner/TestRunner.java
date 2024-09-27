0a835e78ea3ecaa722fbe7bad591802f68360bc0
ADK901 <adarshkariyadan.143@gmail.com>
2024-09-27 10:40:24 +0530

    Partial update commit

--- src/test/java/com/qa/restfulbooker/runner/TestRunner.java
+++ src/test/java/com/qa/restfulbooker/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".//Features", glue = { "com.qa.restfulbooker.stepdefs" }, plugin = {
 		"json:target/jsonReports/cucmber-reports.json", "pretty",
-		"html:test-output.html" }, dryRun = false, tags = "@CreateBooking or @UpdateBooking")
+		"html:test-output.html" }, dryRun = false, tags = "@PartialUpdate")
 public class TestRunner {
 
 }
